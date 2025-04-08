ddeb8ae71a5aba98d7cb75ece2c34b8b66a32518
almaas47 <matt47@byu.edu>
2025-04-08 10:12:59 -0600

    initial commit

--- backend/Intex2/Intex2/obj/Debug/net8.0/Intex2.AssemblyInfo.cs
+++ backend/Intex2/Intex2/obj/Debug/net8.0/Intex2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cae5c3600b05bda8d236574946b3475fb48509e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d661c74ca924c00eb8148ae42ef81216299d3898")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
