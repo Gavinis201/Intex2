72510258dcb71683b514166126f94de4c1a7ce00
Gavin Smith <gavinjacobsmith@gmail.com>
2025-04-08 19:44:10 -0600

    git ignore file

--- backend/Intex2/Intex2/obj/Debug/net8.0/Intex2.AssemblyInfo.cs
+++ backend/Intex2/Intex2/obj/Debug/net8.0/Intex2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ac5bd8bac4efbe1b62c73c2663257d19114140")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f06566cdf328ad52944c03354fc2e9e2018d94f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
