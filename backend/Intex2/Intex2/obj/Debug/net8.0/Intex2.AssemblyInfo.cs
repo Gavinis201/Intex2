fe54c6ce6de1adb966ad9230a94d43deb4ba0b92
Gavin Smith <gavinjacobsmith@gmail.com>
2025-04-08 20:32:00 -0600

    inital

--- backend/Intex2/Intex2/obj/Debug/net8.0/Intex2.AssemblyInfo.cs
+++ backend/Intex2/Intex2/obj/Debug/net8.0/Intex2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df3c26664c0540d32359a55dc3c6655f0d90fa59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9097d60d343cdd7aa1b9a76821d06128f39c4614")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
